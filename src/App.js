162695f6549694dba3048248cbbfd42b059f11c5
habib-sc <Habibur Rahman>
2022-05-10 02:48:25 +0600

    Added home banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
+import Login from './components/Auth/Login/Login';
 import About from './components/Pages/About/About';
 import Home from './components/Pages/Home/Home';
 import Header from './components/Shared/Header/Header';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Pages/Home/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bannerImg from '../../../assets/images/chair.png';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-[60vh] text-accent">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <div className=''>
+                    <img src={bannerImg} class="max-w-sm rounded-lg shadow-2xl" />
+                </div>
+                <div>
+                    <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
