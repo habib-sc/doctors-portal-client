d0d794e0f70d1e25dd23b43dda7a4cd0099d5c53
habib-sc <Habibur Rahman>
2022-05-11 00:35:08 +0600

    Added service section

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Info from './Info/Info';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
         <div className='container mx-auto px-4'>
             <Banner></Banner>
             <Info></Info>
+            <Services></Services>
         </div>
     );
 };
---
+++ src/components/Pages/Home/Services/Services.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import cavityImg from '../../../../assets/images/cavity.png';
+import fluorideImg from '../../../../assets/images/fluoride.png';
+import whiteningImg from '../../../../assets/images/whitening.png';
+import ServiceCard from './ServiceCard/ServiceCard';
+
+const Services = () => {
+
+    const services = [
+        {
+            _id: 1, 
+            name: "Fluoride Treatment",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim ab ipsum et libero nam sit minima sint aperiam,",
+            img: fluorideImg
+
+        },
+        {
+            _id: 2, 
+            name: "Cavity Filling",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim ab ipsum et libero nam sit minima sint aperiam,",
+            img: cavityImg
+
+        },
+        {
+            _id: 3, 
+            name: "Teeth Whitening",
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim ab ipsum et libero nam sit minima sint aperiam,",
+            img: whiteningImg
+
+        },
+    ];
+
+    return (
+        <section className='my-20'>
+            <div className='text-center mb-3'>
+                <h3 className='text-xl font-bold text-secondary uppercase mb-1'>Our Services</h3>
+                <h1 className='text-4xl'>Services We Provide</h1>
+            </div>
+            <div className='grid gird-cols-1 lg:grid-cols-3 gap-4'>
+                {
+                    services.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Services;
