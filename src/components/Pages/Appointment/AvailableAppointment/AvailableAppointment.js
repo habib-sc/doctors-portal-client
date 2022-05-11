193a42c6572a6a66f3bd921081d38f89c134a9c0
habib-sc <Habibur Rahman>
2022-05-12 01:17:31 +0600

    loading servises data from backend

--- src/components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/components/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -8,7 +8,7 @@ const AvailableAppointment = ({selectedDate}) => {
     const [treatment, setTreatment] = useState({});
 
     useEffect( () => {
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
         .then(res => res.json())
         .then(data => setServices(data));
     }, []);
