cd35b9a0b3c0186d55569c8d2840f5257e47fef5
habib-sc <habib97.cs@gmail.com>
2022-05-16 07:38:58 +0600

    making admin form all user page

--- src/components/Pages/Dashboard/AllUsers.js
+++ src/components/Pages/Dashboard/AllUsers.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import Spinner from '../../Shared/Spinner/Spinner';
 
 const AllUsers = () => {
+
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/users', {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+
+    const makeAdmin = email => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            toast.success('Successfully Assigned as Admin');
+            refetch();
+        });
+    };
+
+    if (isLoading) {
+        return <Spinner></Spinner>
+    };
+
     return (
         <div>
-            <h2>All Users</h2>
+            <div class="overflow-x-auto px-4">
+                <h2 className='my-3 text-xl font-semibold'>Total Users: {users?.length}</h2>
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead >
+                        <tr>
+                            <th className='bg-gray-200'>sl</th>
+                            <th className='bg-gray-200'>Email</th>
+                            <th className='bg-gray-200'>Date</th>
+                            <th className='bg-gray-200'>Time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <th>{user.email}</th>
+                                <td>{user.role !== 'admin' && <button onClick={ () => makeAdmin(user.email)} className='btn btn-xs'>Make Admin</button>}</td>
+                                <td><button className='btn btn-xs'>Remove User</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
